a11d984a1dc45c144033f9afdaf85b6b54af8f02
Akash Roy <royaku6@gmail.com>
2022-11-08 21:30:04 +0530

    first commit

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Made with <i className="fas fa-heart" /> by Team 1.
+          {/* <a
+            className="profile"
+            href="https://akashroy.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          > 
+            Akash
+          </a> */}
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
